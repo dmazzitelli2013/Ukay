fd659744ea84431846e1ab0b01c471ca8ef14e21
David Mazzitelli <davidma1984@gmail.com>
2013-04-20 19:19:29 -0300

    project created

---
+++ Ukay/Ukay/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Ukay
+//
+//  Created by David Mazzitelli on 4/20/13.
+//  Copyright (c) 2013 David Mazzitelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Ukay/Ukay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ukay
+//
+//  Created by David Mazzitelli on 4/20/13.
+//  Copyright (c) 2013 David Mazzitelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
